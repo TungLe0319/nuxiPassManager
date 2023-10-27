18858b4fa3a48272c6e2b816ef582352129cb553
tungle0319 <tung.le0319@gmail.com>
2023-10-27 16:30:50 -0600

    setup2

---
+++ components/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="side-bar-container">
+   
+      <v-list-item title="My Application" subtitle="Vuetify"></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item  to="\passwords\1 link title="List Item 1"></v-list-item>
+      <v-list-item link title="List Item 2"></v-list-item>
+      <v-list-item link title="List Item 3"></v-list-item>
+  
+  </div>
+</template>
+<script setup lang="ts">
+const items = [
+  {
+    name: "Item #1",
+    id: 1,
+  },
+  {
+    name: "Item #2",
+    id: 2,
+  },
+  {
+    name: "Item #3",
+    id: 3,
+  },
+];
+</script>
+
+<style scoped>
+.side-bar-container {
+  width: 25l0px;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <SideBar></SideBar>
+  <v-app> </v-app>
+</template>
+
+<script setup lang="ts">
+import SideBar from "../components/SideBar.vue";
+</script>
